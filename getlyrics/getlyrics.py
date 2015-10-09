a6913c8d1ebba8fa4d6e7272c173733440df06eb
Alexander Weigl <weigl@kit.edu>
2015-10-09 21:55:44 +0200

    more support for terminals
    if a terminal is present:
      * the $PAGER is used for displaying the result
      * if the result is ambiguous the user is prompted for a number

--- getlyrics/getlyrics.py
+++ getlyrics/getlyrics.py
@@ -23,6 +23,14 @@ def prompt(*args):
         sys.stdout = old_stdout
 
 
+def pager(string):
+    import subprocess
+    pager_prog = os.environ['PAGER']
+    handle = subprocess.Popen([pager_prog, '-'], stdin=subprocess.PIPE, shell=False)
+    handle.communicate(string)
+    handle.wait()
+
+
 def parse_lyrics_page(soup):
     return [div for div in soup.findAll('div')][22].text.strip()
 
@@ -36,11 +44,13 @@ def get_lyrics(search, index=None, startFromZero=False):
     elif not results:
         return
     elif index is not None:
+        # we found multiple and have an index id to open
         if type(index) in (int, float):
             if not startFromZero:
                 index = index - 1
             return parse_lyrics_page(make_soup(urlopen(results[index].find('a')["href"]).read()))
     else:
+        # we found multiple and have no index to open
         outp = []
         for td in results:
             x = ""
@@ -50,7 +60,14 @@ def get_lyrics(search, index=None, startFromZero=False):
                 else:
                     x += a
             outp.append(x.strip())
-        return "Results:\n" + "\n".join(outp)
+
+        print("Results:", end="\n\t")
+        print("\n\t".join(outp))
+
+        if os.isatty(sys.stdout.fileno()):
+            # stdout is a terminal (interaction possible)
+            index = int(prompt("enter your choice: "))
+            return parse_lyrics_page(make_soup(urlopen(results[index].find('a')["href"]).read()))
 
 
 def main():
@@ -59,7 +76,15 @@ def main():
     parser.add_argument("-i", "--index", help="Specify song index, if multiple results are returned", type=int)
     parser.add_argument("--startFromZero", help="Start index from zero instead of one", action="store_true")
     args = parser.parse_args()
-    print(get_lyrics(args.term, index=args.index, startFromZero=args.startFromZero))
+    lyric = get_lyrics(args.term, index=args.index,
+                       startFromZero=args.startFromZero)
+
+
+    if os.isatty(sys.stdout.fileno()):
+        pager(lyric)
+    else:
+        print(lyric)
+
     return 0
 
 
